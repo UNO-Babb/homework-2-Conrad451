6ccf4da93a12b7f77ed42f274a13a91e9754ba0b
Conrad451 <67305888+Conrad451@users.noreply.github.com>
2025-10-21 20:22:24 +0000

    done

--- BusSchedule.py
+++ BusSchedule.py
@@ -31,6 +31,9 @@ def convert24Hours(time_str):
 
   return hour, minutes
 
+
+  
+
 def loadURL(url):
   """
   This function loads a given URL and returns the text
@@ -71,7 +74,10 @@ def main():
 
   base = "https://myride.ometro.com/Schedule"
   url = f"{base}?stopCode={stopCode}&routeNumber={routeNumber}&directionName={directionName.upper()}" 
-
+  
+  #Codespaces seems to really hate Selenium.
+  #I'm not gonna worry about it for this assignment.
+  
   #c1 = loadURL(url) #loads the web page
   c1 = loadTestPage() #loads the test page
 
@@ -83,20 +89,64 @@ def main():
 
   print(f"Current Time {now_string}")
   
-  #Extract times from the page with some regexing
+  #Extract times from the page with some regexing, storing in a list called busTimes
   busTimes = re.findall(r"\b((?:0?[1-9]|1[0-2]):[0-5]\d ?(?:AM|PM))\b", c1)
  
  
 
   #Initialize our vars for keeping track of the busses.
-  nextBus = None
-  nextDifference = None
+  nextBusTime = None
+  nextBusTimeDifference = None
+
+  """
+  First we calculate the closest bus route. We then call convert24Hours to convert it to 24 hour time.
+  If the busTime is before our now time, we know to skip it, since we get all times for the day.
+
+  
+  """
 
   for time in busTimes:
-    print(time)
     hours, minutes = convert24Hours(time)
-    print(f"{hours}:{minutes}")
+   
+    busTime = now.replace(hour=hours, minute=minutes)
 
+    if busTime < now:
+      continue
+    """
+    Floor divide the time difference into minutes
+    if the difference is less than our current time difference, or one from before, we skip it.
+    Otherwise nextBusTime is set to the difference of busTime and now time.
+    
+    
+    """
+    diff = int((busTime - now).total_seconds() // 60)
+    if nextBusTime is None or diff < nextBusTimeDifference:
+      nextBusTimeDifference = diff
+      nextBusTime = time
+
+  """
+  nextBusTime creates a list of all bus time differences after our nextBus time. 
+  We then take the min from that list to get the lowest difference
+  We also have some basic error handling, if it can't find a bus time it'll spit out a generic error.
+  """
+  if nextBusTime:
+     print(f"The next bus will arrive in {nextBusTimeDifference} minutes.")
+
+     future_difference = []
+     
+     for time in busTimes:
+       
+       hours, minutes = convert24Hours(time)
+       busTime = now.replace(hour=hours, minute=minutes)
+       if busTime > now:
+         diff = int((busTime - now).total_seconds() // 60)
+         if diff > nextBusTimeDifference:
+           future_difference.append(diff)
+
+     if future_difference:
+        print(f"The following bus will arrive in {min(future_difference)} minutes.")
+  else:
+    print("No upcoming busses found.")
 
 
 
